a5e97f728462d29e885f14440b73672c5b6c0b17
fangfangpeng <39129308+fangfangpeng@users.noreply.github.com>
2018-11-20 14:51:17 +0800

    Update useState.jsx

--- useState.jsx
+++ useState.jsx
@@ -4,42 +4,41 @@ let firstRun = true;
 let cursor = 0;
 
 function createSetter (cursor) {
-	return function setterWithCursor (newVal) {
-		state[cursor] = newVal;
-	}
+    return function setterWithCursor (newVal) {
+	state[cursor] = newVal;
+    }
 }
 
 // useState的伪代码实现
 export function useState (initVal) {
-	if (firstRun) {
-		state.push(initVal);
-		setters.push(createSetter(cursor));
-		firstRun = false;
-	} else if (!state[cursor]|| !setters[cursor]) {
-		state.push(initVal);
+    if (firstRun) {
+	state.push(initVal);
+	setters.push(createSetter(cursor));
+	firstRun = false;
+    } else if (!state[cursor]|| !setters[cursor]) {
+	state.push(initVal);
     	setters.push(createSetter(cursor));
-	}
-	const setter = setters[cursor];
-	const value = state[cursor];
-	cursor++;
-	return [value, setter];
+    }
+    const setter = setters[cursor];
+    const value = state[cursor];
+    cursor++;
+    return [value, setter];
 }
 
 function RenderFunctionComponent () {
-	const [firstName, setFirstName] = useState("Rudi"); // cursor: 0
-	const [lastName, setLastName] = useState("Yardley"); // cursor: 1
-	return (
-		<div>
-			<button onClick={() => setFirstName("haha")}></button>
-			<button onClick={() => setLastName("haha")}></button>
-		</div>
-	);
-
+    const [firstName, setFirstName] = useState("Rudi"); // cursor: 0
+    const [lastName, setLastName] = useState("Yardley"); // cursor: 1
+    return (
+	<div>
+	    <button onClick={() => setFirstName("haha")}></button>
+	    <button onClick={() => setLastName("haha")}></button>
+	</div>
+    );
 }
 // 模拟Reacts渲染周期
 function MyComponent () {
-	cursor = 0; //  每次渲染，重置光标的位置
-	return <RenderFunctionComponent />; // render
+    cursor = 0; //  每次渲染，重置光标的位置
+    return <RenderFunctionComponent />; // render
 }
 
 console.log(state); // Pre-render: []
